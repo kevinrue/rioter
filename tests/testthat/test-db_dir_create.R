ddbf2ffda9ba68e45596151071a20c43c331f746
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2021-07-04 18:31:41 +0200

    test that create dir returns FALSE if directory exists already

--- tests/testthat/test-db_dir_create.R
+++ tests/testthat/test-db_dir_create.R
@@ -4,3 +4,8 @@ test_that("db_dir_create() works", {
     out <- rioter::db_dir_create()
     expect_true(out)
 })
+
+test_that("db_dir_create() returns FALSE if it already exists", {
+    out <- rioter::db_dir_create()
+    expect_false(out)
+})
